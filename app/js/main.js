8d25d36b4e78d263ee2850a91f2f4053d3adace1
develeone <babkov@awake.su>
2018-04-22 01:23:28 +1000

    Почти всё

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,74 @@ $(document).ready(function () {
         $(this).addClass("active");
     });
 
+    $(".trainers .item").click(function () {
+        $(this).parent().find(".item").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(document).scroll(function () {
+        SetHeaderOpacity();
+    });
+
+    function SetHeaderOpacity () {
+        var opacity = 1 - $(document).scrollTop().valueOf() / $(window).height();
+        var showing = true;
+
+        if (opacity < 0 && showing) {
+            $(".header").css("display", "none");
+            showing = false;
+        } else if (opacity >= 0 && $(".header").css("display") == "none") {
+            $(".header").css("display", "block");
+            showing = true;
+        } else if (opacity >= 0 && showing) {
+            $(".header").css("opacity", opacity);
+        }
+    }
+
+    SetHeaderOpacity();
+
+    $(".menu-button").click(function () {
+        $(".menu").addClass("active");
+    });
+    $(".menu-close-button").click(function () {
+        $(".menu").removeClass("active");
+    });
+
+
+    $('.trainers .list-container .list').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
+        initialSlide: 1
+    });
+
+
+    $('.reviews .list').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        centerMode: true,
+        initialSlide: 1
+    });
+
+
 });
+
+var agreement = false;
+function switchAgreement() {
+    agreement = !agreement;
+    var checkbox = $(".agreement-checkbox");
+    var button = $(".button-send");
+
+    if (agreement) {
+        $(checkbox).attr("src", "images/checkbox.svg");
+        $(button).removeAttr("disabled");
+    } else {
+        $(checkbox).attr("src", "images/checkbox_unchecked.svg");
+        $(button).attr("disabled", "disabled");
+    }
+}
