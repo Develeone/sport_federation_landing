b4b4ce4f781ce93f70d0cf9a8c79662c21b00fc2
develeone <babkov@awake.su>
2018-04-29 23:49:43 +1000

    Правки #1 (6 шт)

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
 
     $(".target-audience .item").click(function () {
-        $(this).parent().find(".item").removeClass("active");
-        $(this).addClass("active");
+        if($(this).hasClass("active"))
+            $(this).removeClass("active");
+        else {
+            $(this).parent().find(".item").removeClass("active");
+            $(this).addClass("active");
+        }
     });
 
     $(".trainers .item").click(function () {
@@ -48,7 +52,17 @@ $(document).ready(function () {
 
     $(".sale").click(function () {
         $(".modal-callback").addClass("active");
+        $(".inner-container").hide();
     });
+
+
+    $(".recommended-by .prev").click(() => ChangeRecommendedBySlide("prev"));
+    $(".recommended-by .next").click(() => ChangeRecommendedBySlide("next"));
+
+
+    $(".place .prev").click(() => ChangePlaceSlide("prev"));
+    $(".place .next").click(() => ChangePlaceSlide("next"));
+
 
     $('.trainers .list-container .list').slick({
         dots: true,
@@ -86,4 +100,38 @@ function switchAgreement() {
         $(checkbox).attr("src", "images/checkbox_unchecked.svg");
         $(button).attr("disabled", "disabled");
     }
+}
+
+var currentRecommendedBySlideId = 1;
+
+function ChangeRecommendedBySlide(direction) {
+    var recommendedBySlidesCount = $(".recommended-by .slide").length;
+
+    if (recommendedBySlidesCount != $(".recommended-by .pic").length)
+        alert("Attention!\n\nКоличество картинок в слайдере Нас Рекомендуют не соответствует количеству текстовых блоков!");
+
+    if (direction == "next")
+        currentRecommendedBySlideId = currentRecommendedBySlideId < recommendedBySlidesCount ? currentRecommendedBySlideId+1 : 1;
+    else if (direction == "prev")
+        currentRecommendedBySlideId = currentRecommendedBySlideId > 1 ? currentRecommendedBySlideId-1 : recommendedBySlidesCount;
+
+    $(".recommended-by .slide").slideUp();
+    $(".recommended-by .slide[data-id='"+currentRecommendedBySlideId+"']").slideDown();
+
+    $(".recommended-by .pic").fadeOut();
+    $(".recommended-by .pic[data-id='"+currentRecommendedBySlideId+"']").fadeIn();
+}
+
+
+var currentPlaceSlideId = 1;
+function ChangePlaceSlide(direction) {
+    var placeSlidesCount = $(".place .image").length;
+
+    if (direction == "next")
+        currentPlaceSlideId = currentPlaceSlideId < placeSlidesCount ? currentPlaceSlideId+1 : 1;
+    else if (direction == "prev")
+        currentPlaceSlideId = currentPlaceSlideId > 1 ? currentPlaceSlideId-1 : placeSlidesCount;
+
+    $(".place .image").fadeOut();
+    $(".place .image[data-id='"+currentPlaceSlideId+"']").fadeIn();
 }
